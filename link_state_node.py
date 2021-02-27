2172e5f594fb2b8f88fc3c44b4a6605d382bdceb
alejandrodavidmalavet <alejandrodavidmalavet@gmail.com>
2021-02-27 15:02:39 -0600

    LSN Class Fully Implemented

--- link_state_node.py
+++ link_state_node.py
@@ -5,88 +5,124 @@ import json
 class Link_State_Node(Node):
     def __init__(self, id):
         super().__init__(id)
-        self.neighbors = set()
-        self.graph = dict()
-        self.sequence_numbers = dict()
-        
+        self.edges = dict()
+        self.nodes = {self.id : 0}
+        self.dist = {self.id : 0}
+        self.routing_table = {self.id : None}
 
 
-    # Return a string
+
     def __str__(self):
         return "Rewrite this function to define your node dump printout"
 
 
 
-    # Fill in this function
     def link_has_been_updated(self, neighbor, latency):
 
-        self.neighbors.add(neighbor) if latency >= 0 else self.neighbors.remove(neighbor)
+        edge = frozenset([neighbor,self.id])
 
-        sequence_number = self.sequence_numbers[neighbor] + 1 if neighbor in self.sequence_numbers else 0
+        sequence_number = self.edges[edge]['sequence_number'] + 1 if edge in self.edges else 0
 
-        self.sequence_numbers[neighbor] = sequence_numberS
+        self.link(edge,latency,sequence_number)
 
-        edge = frozenset([self.id,neighbor])
+        self.build_routing_table()
 
-        self.add_edge(edge,latency,sequence_number)
-
-        message = json.dumps((  list(edge), 
-                                latency, 
-                                sequence_number))
-
-        self.send_to_neighbors(message)
-    
 
 
     def process_incoming_routing_message(self, m):
 
         edge, latency, sequence_number = json.loads(m)
 
-        # self.neighbors.add(neighbor) if latency >= 0 else self.neighbors.remove(neighbor)
-
-        # sequence_number = self.sequence_numbers[neighbor] + 1 if neighbor in self.sequence_numbers else 0
-
-        # self.sequence_numbers[neighbor] = sequence_number
-
         edge = frozenset(edge)
 
-        if edge not in self.graph or self.graph[edge]['sequence_number'] < sequence_number:
+        if edge not in self.edges or (self.edges[edge]['sequence_number'] < sequence_number):
 
-            self.add_edge(edge,latency,sequence_number)
+            self.build_edge(edge,latency,sequence_number)
 
-            for edge in self.graph:
+            for edge in self.edges:
 
-                message = json.dumps((  list(edge), 
-                                        self.graph[edge]['latency'], 
-                                        self.graph[edge]['sequence_number']))
+                self.link(edge,self.edges[edge]['latency'],self.edges[edge]['sequence_number'])
 
-                self.send_to_neighbors(message)
-
-            self.debug_print()
+            self.build_routing_table()
 
 
 
-    # Return a neighbor, -1 if no path to destination
-    def get_next_hop(self, destination):
-        return -1
+    def build_routing_table(self):
+
+        self.dist = self.nodes.copy()
+
+        nodes_copy = dict()
+
+        while self.nodes:
+
+            node = min(self.nodes, key=self.nodes.get)
+
+            nodes_copy[node] = self.nodes[node]
+
+            del self.nodes[node]
+
+            for edge in self.edges:
+
+                latency = self.edges[edge]['latency']
+
+                if latency >= 0 and node in edge:
+
+                    neighbor = self.get_neighbor(edge,node)
+
+                    alt = self.dist[node] + latency
+
+                    if alt < self.dist[neighbor]:
+
+                        if neighbor in self.nodes: 
+                            self.nodes[neighbor] = alt
+
+                        self.dist[neighbor] = alt
+
+                        self.routing_table[neighbor] = node
+
+        self.nodes = nodes_copy
 
 
 
-    def debug_print(self):
-        print('======================')
-        print('Node:',self.id, '\nNeighbors:',list(self.neighbors))
-        print('======================')
-        for edge in self.graph:
-            nodes = list(edge)
-            print('     Edge:', nodes[0], '&', nodes[1])
-            print('     Latency:', self.graph[edge]['latency'])
-            print('     Sequence Number:', self.graph[edge]['sequence_number'])
-            print('     ------------')
+    def link(self,edge,latency,sequence_number):
+        
+        for node in edge: self.nodes[node] = float('inf') if node != self.id else 0
+
+        self.build_edge(edge,latency,sequence_number)
+
+        message = json.dumps((  list(edge), 
+                                latency, 
+                                sequence_number))
+
+        self.send_to_neighbors(message)
 
 
 
-    def add_edge(self,edge,latency,sequence_number):
-        self.graph[edge] = {
+    def build_edge(self,edge,latency,sequence_number):
+        self.edges[edge] = {
             'latency': latency,
             'sequence_number': sequence_number
         }
+    
+
+
+    def get_neighbor(self,edge,node):
+        edge = set(edge)
+        edge.remove(node)
+        return edge.pop()
+
+
+
+    def debug(self):
+        print("Node:", self.id, "Nodes:",list(self.nodes))
+        for edge in self.edges:
+            print('     Edge:',edge)
+            print('     Latency:', self.edges[edge]['latency'])
+            print('     Sequence Number:',self.edges[edge]['sequence_number'])
+
+
+
+    def get_next_hop(self, destination):
+        while destination in self.routing_table and self.routing_table[destination] is not self.id:
+            destination = self.routing_table[destination]
+        return destination if destination in self.routing_table else -1
